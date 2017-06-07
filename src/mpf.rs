2d429e362539d28e0aee712bad9e6ecc813b00e9
bryant <bryant@users.noreply.github.com>
2017-06-06 21:05:35 -0400

    de-dup

--- src/mpf.rs
+++ src/mpf.rs
@@ -224,11 +224,7 @@ impl fmt::Display for Mpf {
     /// radix point to the left of the first digit. `3.14`, for instance, will
     /// print as `314e1` which means `0.314e1`.
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let mut exp: c_long = 0;
-        match self.get_str(0, 10, &mut exp) {
-            ref n if n.len() == 0 => write!(f, "0"),
-            n => write!(f, "{}e{}", n, exp)
-        }
+        <Self as fmt::Debug>::fmt(self, f)
     }
 }
 
